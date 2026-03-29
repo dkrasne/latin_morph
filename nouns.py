9ad3a2b3501d49aa21f47ff28534e6c326f68e66
dkrasne <dkrasne@gmail.com>
2026-03-29 08:49:17 -0400

    Adjusted noun interface to match other parts of speech; made exact repetition of case/number/declension impossible.

--- nouns.py
+++ nouns.py
@@ -23,7 +23,7 @@ option_expander = st.expander("Settings", expanded=True)
 
 with option_expander:
 
-    col_declension, col_options = st.columns(2)
+    col_declension, col_options = st.columns([3,2])
 
     with col_options:
         st.markdown("Options:", help="You can adjust these options at any point.")
@@ -37,7 +37,8 @@ with option_expander:
 
     with col_declension:
         # radio_change() is defined in utils.py
-        declension = st.radio("Choose a declension to practice:",{"random":"random"} | declension_dict, on_change=radio_change)
+        # declension = st.radio("Choose a declension to practice:",{"random":"random"} | declension_dict, on_change=radio_change)
+        declension = st.multiselect("Choose which declensions to practice (they are all selected by default):", options=list(declension_dict.keys()), default=list(declension_dict.keys()))
 
 
 ## DEFINE AVAILABLE NOUNS AND NOUN ENDINGS ##
@@ -189,150 +190,172 @@ noun_vocab = import_nouns()
 
 # based on radio button 'declension' (which may still need its own key in session_state), filter noun_vocab.
 
-if declension == "random":
-    active_vocab = noun_vocab
-else:
-    active_vocab = {}
-    for noun_key, noun_val in noun_vocab.items():
+active_vocab = {}
+for noun_key, noun_val in noun_vocab.items():
 #        st.write(noun_key)
-        noun_decl = noun_val["decl"]
-        if isinstance(declension_dict[declension], list) and noun_decl in declension_dict[declension]:
-#            st.write("match")
-#            st.write(declension_dict[declension])
+    noun_decl = noun_val["decl"]
+    for decl_sel in declension:
+        if isinstance(declension_dict[decl_sel], list) and noun_decl in declension_dict[decl_sel]:
+    #            st.write("match")
+    #            st.write(declension_dict[declension])
             active_vocab = active_vocab | {noun_key: noun_val}
-        elif noun_decl == declension_dict[declension]:
-#            st.write("match")
+        elif noun_decl == declension_dict[decl_sel]:
+    #            st.write("match")
             active_vocab = active_vocab | {noun_key: noun_val}
         else:
-#            st.write("no match")
+    #            st.write("no match")
             continue
 #    st.write(noun_vocab)
 
+# st.write(st.session_state.question_list)
 ## CREATE THE QUIZ ##
-
-def gen_question():
-    noun = random.choice(list(active_vocab.keys()))
-    number = random.choice(list(noun_options["number"].keys()))
-    if number == "sg":
-        case = random.choices(list(noun_options["case"].keys()),[10,90,90,90,90,90])[0]
-    else:
-        case = random.choice(list(noun_options["case"].keys()))
-
-    return [noun, case, number]
-
-st.session_state.gen_func = gen_question
-
-if st.session_state.current_question:
-    noun, case, number = st.session_state.current_question
-
-    # # Uncomment for testing purposes.
-    # st.write(st.session_state.current_question)
-    # st.write(f"Give the {noun_options["case"][case]} {noun_options["number"][number]} of *{noun}*.")
-
-    noun_decl = active_vocab.get(noun, {}).get("decl")
-
-    if case == "nom" and number == "sg":    # nominative singulars don't choose from list
-        correct_answer = noun
-    else:
-        correct_ending = noun_endings[noun_decl][number][case]
-
-        if correct_ending is None and number == "sg":   # deal with vocative singular other than 2nd decl. -us nouns
-            correct_answer = noun
+if len(declension) == 0 and not st.session_state.current_question:
+    st.write("You need to choose at least one declension.")
+else:
+    def gen_question():
+        last_question = st.session_state.question_list[-1]
+        decl_rand = random.choice(declension)
+        decl_dict_subset = declension_dict.get(decl_rand)
+        # st.write(decl_dict_subset)
         
+        if isinstance(decl_dict_subset, list):
+            vocab_subset = {k: v for k,v in active_vocab.items() if v["decl"] in decl_dict_subset}
         else:
-            if correct_ending is None and number == "pl":   # deal with vocative plurals
-                correct_ending = noun_endings[noun_decl][number]["nom"]
-                correct_answer = active_vocab[noun]["stem"] + correct_ending
-            elif isinstance(correct_ending, list):    # deal with alternative forms
-                #correct_ending = correct_ending[0]
-                correct_answer = []
-                for ending in correct_ending:
-                    correct_answer.append(active_vocab[noun]["stem"] + ending)
+            vocab_subset = {k: v for k,v in active_vocab.items() if v["decl"] == decl_dict_subset}
+        noun = random.choice(list(vocab_subset.keys()))
+        number = random.choice(list(noun_options["number"].keys()))
+        if number == "sg":
+            case_weights = [10,90,90,90,90]
+            if decl_rand == "2nd":
+                case_weights.append(80)
             else:
-                correct_answer = active_vocab[noun]["stem"] + correct_ending
+                case_weights.append(10)
+        else:
+            case_weights = [90,90,90,90,90,10]
+        case = ""
+        if noun_vocab[noun]["decl"] == last_question["id"].get("decl") and number == last_question["id"].get("num"):
+            case = last_question["id"].get("case")
+        while case == "" or case == last_question["id"].get("case"):
+            case = random.choices(list(noun_options["case"].keys()),case_weights)[0]
 
-    st.session_state["correct_answer"] = correct_answer
+        # st.write(noun, case, number)
+        return [noun, case, number]
 
-    question = f"For *{noun}*, give the **{noun_options["case"][case]} {noun_options["number"][number]}**."
+    st.session_state.gen_func = gen_question
 
-    if show_declension:
-        for key, val in declension_dict.items():
-            if isinstance(val, list):
-                if noun_decl in val:
-                    decl = key
-                if key == "3rd":
-                    if noun_decl == "3_istem":
-                        third_logic = "i-stem "
-                    elif noun_decl == "3_neut":
-                        third_logic = "neuter "
-                    elif noun_decl == "3_istem_neut":
-                        third_logic = "neuter i-stem "
+    if st.session_state.current_question:
+        noun, case, number = st.session_state.current_question
+
+        # # Uncomment for testing purposes.
+        # st.write(st.session_state.current_question)
+        # st.write(f"Give the {noun_options["case"][case]} {noun_options["number"][number]} of *{noun}*.")
+
+        noun_decl = noun_vocab.get(noun, {}).get("decl")
+
+        if case == "nom" and number == "sg":    # nominative singulars don't choose from list
+            correct_answer = noun
+        else:
+            correct_ending = noun_endings[noun_decl][number][case]
+
+            if correct_ending is None and number == "sg":   # deal with vocative singular other than 2nd decl. -us nouns
+                correct_answer = noun
+            
+            else:
+                if correct_ending is None and number == "pl":   # deal with vocative plurals
+                    correct_ending = noun_endings[noun_decl][number]["nom"]
+                    correct_answer = noun_vocab[noun]["stem"] + correct_ending
+                elif isinstance(correct_ending, list):    # deal with alternative forms
+                    #correct_ending = correct_ending[0]
+                    correct_answer = []
+                    for ending in correct_ending:
+                        correct_answer.append(active_vocab[noun]["stem"] + ending)
+                else:
+                    correct_answer = noun_vocab[noun]["stem"] + correct_ending
+
+        st.session_state["correct_answer"] = correct_answer
+
+        question = f"For *{noun}*, give the **{noun_options["case"][case]} {noun_options["number"][number]}**."
+
+        if show_declension:
+            for key, val in declension_dict.items():
+                if isinstance(val, list):
+                    if noun_decl in val:
+                        decl = key
+                    if key == "3rd":
+                        if noun_decl == "3_istem":
+                            third_logic = "i-stem "
+                        elif noun_decl == "3_neut":
+                            third_logic = "neuter "
+                        elif noun_decl == "3_istem_neut":
+                            third_logic = "neuter i-stem "
+                        else:
+                            third_logic = ""
                     else:
-                        third_logic = ""
+                        pass
                 else:
-                    pass
-            else:
-                if noun_decl == val:
-                    decl = key
-                else:
-                    pass
-        question += f" This is a {decl} declension {third_logic}noun."
+                    if noun_decl == val:
+                        decl = key
+                    else:
+                        pass
+            question += f" This is a {decl} declension {third_logic}noun."
 
-    if show_stem:
-        question += f" (The base is: {active_vocab[noun]["stem"]}-)"
+        if show_stem:
+            question += f" (The base is: {active_vocab[noun]["stem"]}-)"
 
-    st.markdown("### Current question")
+        st.markdown("### Current question")
 
-    with st.form(key="noun_form", clear_on_submit=True):
-        current_answer = st.text_input(question, key="answer_input")
-        
-        submit_button_col, user_answer_col = st.columns([1,2])
-        with submit_button_col:
-            def disable_button():
-                    st.session_state.button_disable = True
-            st.form_submit_button(
-                "Check Answer", 
-                key="form_submission_button",
-                on_click=submit_and_check_answer,
-                disabled=st.session_state.button_disable,
+        with st.form(key="noun_form", clear_on_submit=True):
+            current_answer = st.text_input(question, key="answer_input")
+            
+            submit_button_col, user_answer_col = st.columns([1,2])
+            with submit_button_col:
+                def disable_button():
+                        st.session_state.button_disable = True
+                st.form_submit_button(
+                    "Check Answer", 
+                    key="form_submission_button",
+                    on_click=submit_and_check_answer,
+                    disabled=st.session_state.button_disable,
+                )
+            with user_answer_col:
+                st.markdown(st.session_state.answer_display_message)
+
+        curr_question = {
+                "pos": "noun",
+                "word": noun, 
+                "id": {
+                    "case": case,
+                    "num": number,
+                    "decl": "1st" if str(noun_decl)[0] == "1" else "2nd" if str(noun_decl)[0] == "2" else "3rd (i-stem)" if "istem" in str(noun_decl) else "3rd" if str(noun_decl)[0] == "3" else "4th" if str(noun_decl)[0] == "4" else "5th"
+                },
+    #            "correct": False
+            }
+
+        if st.session_state.append_answer is True:
+            questions_asked.append(
+                curr_question
             )
-        with user_answer_col:
-            st.markdown(st.session_state.answer_display_message)
+            st.session_state.append_answer = False
 
-    curr_question = {
-            "pos": "noun",
-            "word": noun, 
-            "id": {
-                "case": case,
-                "num": number,
-                "decl": "1st" if str(noun_decl)[0] == "1" else "2nd" if str(noun_decl)[0] == "2" else "3rd (i-stem)" if "istem" in str(noun_decl) else "3rd" if str(noun_decl)[0] == "3" else "4th" if str(noun_decl)[0] == "4" else "5th"
-            },
-#            "correct": False
-        }
+    ## GENERATE NEW QUESTIONS AND CHECK ANSWERS ##
 
-    if st.session_state.append_answer is True:
-        questions_asked.append(
-            curr_question
-        )
-        st.session_state.append_answer = False
+    new_question_col, results_col, score_col = st.columns(3)
 
-## GENERATE NEW QUESTIONS AND CHECK ANSWERS ##
+    with new_question_col:
+        st.button("New Question", on_click=new_question, args=(gen_question,), key="question_button", width="stretch", 
+                  disabled=True if len(declension) == 0 else False
+                  )
 
-new_question_col, results_col, score_col = st.columns(3)
+    with results_col:
+        st.markdown(st.session_state.result_message)    # just write the result message, rather than other things as well.
 
-with new_question_col:
-    st.button("New Question", on_click=new_question, args=(gen_question,), key="question_button", width="stretch")
+    with score_col:
+        st.button("Reset Score", "reset", on_click=reset, width="stretch")
+        st.markdown(f"Current score: **{st.session_state.current_score}** out of **{st.session_state.total_questions}**")
 
-with results_col:
-    st.markdown(st.session_state.result_message)    # just write the result message, rather than other things as well.
+    if st.session_state.auto_advance_trigger and st.session_state.answer_checked:
+        time.sleep(st.session_state.auto_advance)
+        new_question(st.session_state.gen_func)
+        st.rerun()
 
-with score_col:
-    st.button("Reset Score", "reset", on_click=reset, width="stretch")
-    st.markdown(f"Current score: **{st.session_state.current_score}** out of **{st.session_state.total_questions}**")
-
-if st.session_state.auto_advance_trigger and st.session_state.answer_checked:
-    time.sleep(st.session_state.auto_advance)
-    new_question(st.session_state.gen_func)
-    st.rerun()
-
-#st.write(questions_asked)
+    #st.write(questions_asked)
