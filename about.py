e2da874822f4a45f9192447874f52d9d7cf9d865
dkrasne <dkrasne@gmail.com>
2026-04-11 16:10:47 -0400

    Swapped order of bibliography and adaptive learning info on About page

--- about.py
+++ about.py
@@ -18,6 +18,22 @@ st.markdown("""
             """, unsafe_allow_html=True)
 
 st.markdown("""
+            ### About the Adaptive Learning Algorithm
+
+            Adaptive learning is currently in the process of being implemented so that words and/or forms you struggle with are more likely to recur.
+            So far, it has been implemented on **pronouns** and **verbal adjectives**.
+
+            There is a 1 in 4 chance that instead of being asked for a completely random form, 
+            you'll be asked for a word or form that you have previously gotten wrong more than a (proportionally) small number of times. 
+            The precise calculation depends on the part of speech, but it's similar to the way that "Most in Need of Review" is calculated on the Session Stats & Data page: 
+            it's based on the number of times that a word/form has been incorrectly produced and the number of times that word/form has been asked for, 
+            while "Most in Need of Review" also takes into account the overall number of answers and incorrect answers.
+            (If you're curious about the specifics, you can look for the `## ADAPTIVE LEARNING ALGORITHM ##` 
+            comment in the code for a given part of speech, on the GitHub site.)
+
+            """)
+
+st.markdown("""
             ### Bibliography
 
             Generally speaking, the specifications for irregular forms and non-standard patterns adhere to grammatical rules as outlined by 
@@ -33,19 +49,3 @@ st.markdown("""
             - Rauk, J. (1997) "The Vocative of Deus and Its Problems," *Classical Philology* 92: 138-149
 
             """, unsafe_allow_html=True)
-
-st.markdown("""
-            ### About the Adaptive Learning Algorithm
-
-            Adaptive learning is currently in the process of being implemented so that words and/or forms you struggle with are more likely to recur.
-            So far, it has been implemented on **pronouns** and **verbal adjectives**.
-
-            There is a 1 in 4 chance that instead of being asked for a completely random form, 
-            you'll be asked for a word or form that you have previously gotten wrong more than a (proportionally) small number of times. 
-            The precise calculation depends on the part of speech, but it's similar to the way that "Most in Need of Review" is calculated on the Session Stats & Data page: 
-            it's based on the number of times that a word/form has been incorrectly produced and the number of times that word/form has been asked for, 
-            while "Most in Need of Review" also takes into account the overall number of answers and incorrect answers.
-            (If you're curious about the specifics, you can look for the `## ADAPTIVE LEARNING ALGORITHM ##` 
-            comment in the code for a given part of speech, on the GitHub site.)
-
-            """)
